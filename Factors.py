7676cddeb4252bd02a34af7917232fc19849c00e
Gokul <K.Gokulkrishn@gmail.com>
2019-11-22 16:35:58 +0530

    adding factors

---
+++ Factors.py
@@ -0,0 +1,8 @@
+def Factors(num):
+    Factors_list = []
+    for i in range(1,num):
+        if (num%i ==0):
+            Factors_list.append(i)
+    return Factors_list
+
+print(Factors(10))
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from Factors import Factors
 def GCD(num1,num2):
+    print('came',num1,num2)
     if num2>num1:
         (num2,num1) = (num1,num2)
+    if num1 != num2:
+        # print(num1,num2)
+        GCD(num1-num2,num2)
     for i in range(num2+1,0,-1):
         if (num1%i==0 and num2%i==0):
             return i
 
-print(GCD(3,15))
+# print(GCD(68,150))
+
+Factors(10)
+
