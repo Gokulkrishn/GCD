75271ce1adcad5dcf6586863c286f41007d51348
Gokul <K.Gokulkrishn@gmail.com>
2019-11-22 17:29:44 +0530

    removed junk

--- primes.py
+++ primes.py
@@ -1,11 +1,7 @@
 ## returns list of all primes
-<<<<<<< HEAD
-
 def Primes(num):
     for i in range(2,num):
         if (num%i == 0):
             return False
     else:
         return True
-=======
->>>>>>> 9010f37aa051d1737346ec561709351499916aba
